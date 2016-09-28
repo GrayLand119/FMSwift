8fd47cc215520884e960078cb8c095978fd4337e
Guilin Lan <441726442@qq.com>
2016-09-28 14:41:52 +0800

    Add FMSwfit.framework Release for iOS Platform

---
+++ Moudle/Framework/FMSwift.framework/Headers/FMSwift.h
@@ -0,0 +1,23 @@
+//
+//  FMSwift.h
+//  FMSwift
+//
+//  Created by GrayLand on 16/9/19.
+//  Copyright © 2016年 GrayLand Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FMSwift.
+FOUNDATION_EXPORT double FMSwiftVersionNumber;
+
+//! Project version string for FMSwift.
+FOUNDATION_EXPORT const unsigned char FMSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FMSwift/PublicHeader.h>
+
+//#import "GLOCTest.h"
+
+#import "IntelHex2BinConverter.h"
+
+
---
+++ Moudle/Framework/FMSwift.framework/Headers/GLOCTest.h
@@ -0,0 +1,17 @@
+
+//
+//  GLOCTest.h
+//  FMSwift
+//
+//  Created by GrayLand on 16/9/19.
+//  Copyright © 2016年 GrayLand Studio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface GLOCTest : NSObject
+
+- (void)disp;
+
+@end
