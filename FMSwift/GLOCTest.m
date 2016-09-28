6c1c5caa50b35c2ed377b3c8c77a688efb9c1de1
Guilin Lan <441726442@qq.com>
2016-09-28 14:22:16 +0800

    Publish

---
+++ FMSwift/GLOCTest.m
@@ -0,0 +1,19 @@
+//
+//  GLOCTest.m
+//  FMSwift
+//
+//  Created by GrayLand on 16/9/19.
+//  Copyright © 2016年 GrayLand Studio. All rights reserved.
+//
+
+#import "GLOCTest.h"
+
+@implementation GLOCTest
+
+
+- (void)disp
+{
+    NSLog(@"12312312312312");
+}
+
+@end
---
+++ FMSwift/GLTest.swift
@@ -0,0 +1,15 @@
+//
+//  GLTest.swift
+//  FMSwift
+//
+//  Created by GrayLand on 16/9/19.
+//  Copyright © 2016年 GrayLand Studio. All rights reserved.
+//
+
+import UIKit
+
+public class GLTest: NSObject {
+    @objc public func disp() {
+        print("GLTest")
+    }
+}
